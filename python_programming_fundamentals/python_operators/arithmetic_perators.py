d3804a386810ad6007df5cdf850128a68cc07715
Sindhuja N <90563382+sindhujan24@users.noreply.github.com>
2021-11-26 11:53:55 +0530

    Create arithmetic_perators.py

---
+++ python_programming_fundamentals/python_operators/arithmetic_perators.py
@@ -0,0 +1,148 @@
+# Arithematic Operators
+Arithematic Operators are used for performing basic arithematic operations in Python.
+
+## 1. Addition Operator
+Addition operator adds two values.
+### x+y
+
+# Adding two numbers
+5+8
+
+#Adding two numbers
+100+246
+
+## 2. Subtraction Operator
+Subtraction operator subtracts one value from the other.
+### x-y
+
+# Subtracting
+50-46
+
+# Subtracting
+2867-365
+
+## 3. Multiplication Operator
+Multiplication operator multiplies two values.
+### x*y
+
+#multiplies two values
+45*34
+
+387*4
+
+## 4. Dividend Operator
+Dividend operator divides one operand by another.
+### x/y
+
+45/3
+
+764/56
+
+## 5. Modulus Operator
+Modulus operator returns remainder of the division
+### x%y
+
+456%23
+
+87645%8
+
+## 6. Exponent Operator
+Exponent Operator raises the power of left operand to the right operand.
+### x**y
+
+7**4
+
+32**3
+
+## 7. Floor Division Operator
+Floor division operator returns quotient rounded off to the nearest integer.
+### x//y
+
+764//56
+
+8746//14
+
+# Assignment Operators
+Assignment operators are used to assign a value to the variable.
+
+## 1. Equals to (=) Operator
+Equals to (=) operator assigne the value of right hand operand to the left hand operand.
+
+x = 7
+y = 3.65
+
+## 2. Add Equal to (+=) Operator
+Add equal to (+=) Operator adds right hand operand to the left hand operand and assign the result to the left hand operand.
+###  x+=y       x=x+y 
+
+x = 7
+y = 4
+x+=y
+x
+
+## 3. Subtract Equal to (-=) Operator
+Subtract equal to (-=) operator subtracts right hand operand from the left hand operand and assign the result to the left hand operand.
+### x-=y  x=x-y
+
+x = 45
+y = 12
+x-=y
+x
+
+## 4. Mutliply Equal to (*=) Operator
+Multiply equal to (*=) operator multiplies right hand operand by left hand operand and assign the result to the left hand operand.
+### x*=y x=x*y
+
+x = 23
+y = 4
+x*=y
+x
+
+## 5. Divide Equal to (/=) Operator
+Divide equal to (/=) operator divides left operand by right operand and assign the result to left hand operand.
+### x/=y    x=x/y
+
+x = 35
+y = 5
+x/=y
+x
+
+## 6. Modulus Equal to (%=) Operator
+Modulus equal to (%=) operator takes the module of the left operand and, using the right operand, stores the result in the left operand.
+### x%=y    x=x%y
+
+x = 456
+y = 23
+x%=y
+x
+
+## 7. Exponent Equal to (**=) Operator
+Exponent equal to (****=) operator raises the exponential power of the left operand to the value of the right operand, storing the result in the left operand.
+### x**=y   x=x**y
+
+
+x = 7
+y = 4
+x**=y
+x
+
+## 8. Floor Equal to (//=) Operator
+Floor equal to (//=) operator  divides the value found in the left operand by the value of the right operand. The integer result will be placed in the left operand.
+### x//=y    x=x//y
+
+x = 764
+y = 56
+x//=y
+x
+
+# Ninja Trick!!!
+
+
+x = 10
+x+5
+x
+
+x = 10
+x+=5
+x
+
